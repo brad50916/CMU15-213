bc3d3a4631f5f11c231652a287c5cfa7cd4eaca9
bard <bead50916@gmail.com>
2023-04-01 15:08:31 +0800

    test push to github

--- cprogramminglab-handout/queue.h
+++ cprogramminglab-handout/queue.h
@@ -28,7 +28,7 @@ typedef struct list_ele {
      * whenever an element is inserted and removed from the queue.
      */
     char *value;
-
+	
     /**
      * @brief Pointer to the next element in the linked list.
      */
